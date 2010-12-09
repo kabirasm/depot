3e07016c439a8da2b04a1aba8f1357d81f47a763
Kabir Mohammed <kabirasm@gmail.com>
2010-12-09 14:51:17 +0000

    Fixed mail

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -65,7 +65,7 @@ class OrdersController < ApplicationController
       if @order.save
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
-        Notifier.order_received(@order).deliver
+      #  Notifier.order_received(@order).deliver
         format.html { redirect_to(store_url, :notice => 
           'Thank you for your order.') }
         format.xml  { render :xml => @order, :status => :created,
